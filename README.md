dfd59847d4182a0adea194ca5f598c27300feeaa
Lulu Gan <melinda@Melinda.hawaii.rr.com>
2023-01-24 12:18:42 -1000

    readme

---
+++ README.md
@@ -0,0 +1,53 @@
+# What is LOCO-MP?
+
+
+The LOCO-MP python package provide a mostly model-agnostic and distribution-free inference
+framework for feature importance that is applicable to both regression and classification tasks and is computationally efficient and statistically powerful. 
+The motivation for this method is described in Gan, L., Zheng, L., & Allen, G. I. (2022) [link to preprint](https://arxiv.org/abs/2206.02088). Please visit https://DataSlingers.github.io/LOCOMP_paper for code to reproduce the results from our paper.
+
+ 
+
+## Installation
+
+LOCOMP 0.1 and later require Python 3.7 or Python 3.8. 
+
+Clone this repo and run
+
+    python setup.py install
+    
+**The dependencies of this package will be automatically installed into your environment.**
+
+
+
+## Base estimator requirement 
+
+The LOCO-MP function requires base estimator functions which takes arguments (training data matrix X, training response Y, testing data matrix X1) as input and predictions of X1 as output for regression; (training data matrix X, training response Y) as as input and fitted model as output for classification. Users can import base estimators including ridge, random forest and support vector machine from MLmodels. Examples: 
+
+##### Ridge regression function:
+
+```
+def ridge2(X,Y,X1):
+    clf = Ridge(fit_intercept = False,alpha=0.001).fit(X, Y)    
+    return clf.predict(X1)
+```
+    
+##### Logistic regression function:     
+
+```
+def logitridge(X,Y):
+    fit=LogisticRegression(penalty='l2',solver='saga',max_iter=10,C = 1000).fit(X,Y)
+    return fit
+``` 
+    
+
+
+## API Documentations
+
+The detailed API documentation for this package can be found at [https://DataSlingers.github.io/LOCOMP]
+
+
+## References
+
+Gan L, Zheng L, Allen G I. Inference for Interpretable Machine Learning: Fast, Model-Agnostic Confidence Intervals for Feature Importance[J]. arXiv preprint arXiv:2206.02088, 2022.
+
+
--- locomp/MLmodels.py
+++ locomp/MLmodels.py
@@ -11,24 +11,17 @@ def DecisionTreeReg(X,Y,X1):
     tree = DecisionTreeRegressor().fit(X,Y)
     return tree.predict(X1)
 def ridgecv(X,Y,X1):
-    clf = RidgeCV(alphas=[1e-3, 1e-2, 1e-1, 1], fit_intercept = False).fit(X, Y)
-    # named ridge2 to distinguish it from the ridge regressor in sklearn.
-    
+    clf = RidgeCV(alphas=[1e-3, 1e-2, 1e-1, 1], fit_intercept = False).fit(X, Y)    
     return clf.predict(X1)
 
 def ridge2(X,Y,X1):
-    clf = Ridge(fit_intercept = False,alpha=0.001).fit(X, Y)
-    # named ridge2 to distinguish it from the ridge regressor in sklearn.
-    
+    clf = Ridge(fit_intercept = False,alpha=0.001).fit(X, Y)    
     return clf.predict(X1)
 
 def RFreg(X,Y,X1):
-    # when bootstrap=False, it means each tree is trained on all rows of X and only
-    #      subsamples its columns (which are features).
     rf = RandomForestRegressor(n_estimators=200,criterion='mse',bootstrap=False).fit(X,Y)
     return rf.predict(X1)
 
-# kernel svm 
 def kernelSVR(X,Y,X1):
     krr = SVR(kernel='rbf',C=1000).fit(X, Y)
     return krr.predict(X1)
@@ -44,18 +37,17 @@ from sklearn.ensemble import RandomForestClassifier
 from  sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import LogisticRegressionCV
 def logitridge(X,Y):
-    reg=LogisticRegression(penalty='l2',solver='saga',max_iter=10,C = 1000).fit(X,Y)
-    return reg
-# Define Baseline Regressor to be used later
+    fit=LogisticRegression(penalty='l2',solver='saga',max_iter=10,C = 1000).fit(X,Y)
+    return fit
+
 def DecisionTreeClass(X,Y):
     tree = DecisionTreeClassifier(min_samples_leaf=5).fit(X,Y)
     return tree
 
 
 def logitridgecv(X,Y):
-    reg=LogisticRegressionCV(cv=5,penalty='l2',solver='saga').fit(X,Y)
-    # named ridge2 to distinguish it from the ridge regressor in sklearn.
-    return reg
+    fit=LogisticRegressionCV(cv=5,penalty='l2',solver='saga').fit(X,Y)
+    return fit
 
 def RFclass(X,Y):
     M =len(X[0])    # when bootstrap=False, it means each tree is trained on all rows of X and only
